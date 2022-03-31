fa22546e55c55587f353c8ad3957b9169c5eb40b
luanaflorencio <lua.m.016@gmail.com>
2022-03-31 16:37:28 -0300

    Criaçao e configuraçao da tabela de Tarefas, seus campos e sua Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,8 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function tarefa() {
+        return $this->hasMany('App\Models\Tarefa', 'foreign_key', 'user_id');
+    }
 }
