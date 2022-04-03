4143434031d98be6ccf5c4c917f0726e2b0d2fb3
luanaflorencio <lua.m.016@gmail.com>
2022-04-02 23:12:30 -0300

    Exibindo Descriçao ao clicar na seta, adicionando botao para redirecionar para creaçao de tarefas

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -4,12 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Tarefa;
+use App\Models\User;
 
 class DashboardController extends Controller
 {
     public function dash()
     {
-        $tarefa = Tarefa::orderBy('created_at', 'desc')->get();
+        $tarefa = Tarefa::orderBy('tarefas.created_at', 'desc')->join('users', 'users.id', 'tarefas.user_id')->get();
         return view('dashboard', compact('tarefa'));
     }
 }
--- resources/views/tarefas/index.blade.php
+++ resources/views/tarefas/index.blade.php
@@ -2,59 +2,69 @@
 
 <!DOCTYPE html>
 <head></head>
-<body  class="bg-gray-200 flex items-center justify-center h-screen">
+    <body  class="bg-gray-200 flex items-center justify-center h-screen">
+        <a href="" class="btn btn-primary mb-4">
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#FFFFFF"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6-6-6z"/></svg>
+            Criar uma nova tarefa</a><br>
     
+        @foreach ($tarefa as $trf)
+        {{ $trf->user->name }}
         
-    
-    @foreach ($tarefa as $trf)
-    {{ $trf->user_id }}
-    
-    <div class="p-3 border mt-4 mb-5">
-        <strong>Nome da Tarefa: </strong>
-        <p>{{ $trf->nome}}</p>
+        <div class="p-3 border mt-4 mb-5">
+            <strong>Nome da Tarefa: </strong>
+            <p>{{ $trf->nome}}</p>
 
-        <strong>Prioridade: </strong>
-        <p>
+            <strong>Prioridade: </strong>
+            <p>
+                @php
+                if($trf->prioridade == "baixa"){
+                    $prioridade = "Baixa";
+                }
+                else if ($trf->prioridade == "media"){
+                    $prioridade = "Média";
+                }
+                else if($trf->prioridade == "alta"){
+                    $prioridade = "Alta";
+                }
+                @endphp
+                {{ $prioridade}}</p>
+
+            <strong>Tarefa Concluída? </strong>
+            <p>
             @php
-            if($trf->prioridade == "baixa"){
-                $prioridade = "Baixa";
+            if($trf->finalizada == "1"){
+                $finalizada = "Sim";
             }
-            else if ($trf->prioridade == "media"){
-                $prioridade = "Média";
-            }
-            else if($trf->prioridade == "alta"){
-                $prioridade = "Alta";
+            else if ($trf->finalizada == "0"){
+                $finalizada = "Não";
             }
             @endphp
-            {{ $prioridade}}</p>
-
-        <strong>Tarefa Concluída? </strong>
-        <p>
-           @php
-           if($trf->finalizada == "1"){
-               $finalizada = "Sim";
-           }
-           else if ($trf->finalizada == "0"){
-               $finalizada = "Não";
-           }
-           @endphp
-            {{ $finalizada }}</p> 
-                <a href="{{ route('edit-tarefa' , ['id'=>$trf->id]) }}" style="border-color: #194765; margin-bottom: 10px;" class="btn btn-light">
-                    <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="17px" viewBox="0 0 24 24" width="24px" fill="#194765"><g><rect fill="none" height="24" width="24"/></g><g><g><g><path d="M3,21l3.75,0L17.81,9.94l-3.75-3.75L3,17.25L3,21z M5,18.08l9.06-9.06l0.92,0.92L5.92,19L5,19L5,18.08z"/></g><g><path d="M18.37,3.29c-0.39-0.39-1.02-0.39-1.41,0l-1.83,1.83l3.75,3.75l1.83-1.83c0.39-0.39,0.39-1.02,0-1.41L18.37,3.29z"/></g></g></g></svg></a>
-               <a href="" style="border-color: #BB2D3B; margin-bottom: 10px;" class="btn btn-light">
-                   <svg xmlns="http://www.w3.org/2000/svg" height="17px" viewBox="0 0 24 24" width="24px" fill="#BB2D3B"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"/></svg></a>
-            
-            <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 0 24 24" width="30px" fill="#000000"><path d="M24 24H0V0h24v24z" fill="none" opacity=".87"/><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z"/></svg>  
-           
-           
-    </div>
+                {{ $finalizada }}</p> 
+                    <a href="{{ route('edit-tarefa' , ['id'=>$trf->id]) }}" style="border-color: #194765; margin-bottom: 10px;" class="btn btn-light">
+                        <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="17px" viewBox="0 0 24 24" width="24px" fill="#194765"><g><rect fill="none" height="24" width="24"/></g><g><g><g><path d="M3,21l3.75,0L17.81,9.94l-3.75-3.75L3,17.25L3,21z M5,18.08l9.06-9.06l0.92,0.92L5.92,19L5,19L5,18.08z"/></g><g><path d="M18.37,3.29c-0.39-0.39-1.02-0.39-1.41,0l-1.83,1.83l3.75,3.75l1.83-1.83c0.39-0.39,0.39-1.02,0-1.41L18.37,3.29z"/></g></g></g></svg></a>
+                <a href="" style="border-color: #BB2D3B; margin-bottom: 10px;" class="btn btn-light">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="17px" viewBox="0 0 24 24" width="24px" fill="#BB2D3B"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M16 9v10H8V9h8m-1.5-6h-5l-1 1H5v2h14V4h-3.5l-1-1zM18 7H6v12c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7z"/></svg></a>
+                    
+                    <x-dropdown>
+                        <x-slot name="trigger">
+                            <button>
+                                <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 0 24 24" width="30px" fill="#000000"><path d="M24 24H0V0h24v24z" fill="none" opacity=".87"/><path d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6-1.41-1.41z"/></svg>  
+                            </button>
+                        </x-slot>
     
-@endforeach
-    
-       
+                        <x-slot name="content">
+                            <div style="text-align: center;">
+                            <strong>Descrição: </strong>
+                            <p>{{ $trf->descricao}}</p>
+                            </div>
+                        </x-slot>
+                    </x-dropdown>
+        </div>
+        
+    @endforeach
    
-</body>
-
+    </body>
+</html>
 <script src="https://cdn.jsdeli.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
